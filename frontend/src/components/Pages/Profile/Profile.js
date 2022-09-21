6e7448ce8c19d6ce2059dc0cdb5d28cda290c12c
markiyanty <markiyan.tyndyk@gmail.com>
2022-09-21 14:38:44 +0300

    new file system added

--- frontend/src/components/Pages/Profile/Profile.js
+++ frontend/src/components/Pages/Profile/Profile.js
@@ -1,20 +1,43 @@
 import React from "react";
-import Header from "../Header";
-import Info from "../Info";
+import { Form, Button, Container } from "react-bootstrap";
+
+
 
 
 
 const Profile =  () => {
 
     return (
-      <div className="Profile">
-        
-      <Header/>
-      <div>
-        <p>PIZSKSKSKSK</p>
-      </div>
-      <Info/>
-      </div>
+        <>
+      <Container className="mt-3" style={{
+        width: '45%', height: '55%'
+      }}>
+      <Form>
+      <fieldset >
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
+          <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+          <Form.Select id="disabledSelect">
+            <option>Disabled select</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Check
+            type="checkbox"
+            id="disabledFieldsetCheck"
+            label="Can't check this"
+          />
+        </Form.Group>
+        <Button type="submit">Submit</Button>
+      </fieldset>
+    </Form>
+      </Container>
+     <div>
+     </div>
+     </>
     );
   }
   
